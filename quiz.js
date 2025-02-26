b40d2b691a08ae24ac4831bde07e7c59ee3ccd4d
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-02-26 22:24:21 +0100

    Javascript file of Simple Quiz

---
+++ quiz.js
@@ -0,0 +1,32 @@
+function checkAnswer() {
+    let correctAnswer = '4';
+
+    let userAnswer = document.querySelector('input[name="quiz"]:checked');
+
+    let feedback = document.getElementById("feedback");
+
+    if (!userAnswer) {
+        feedback.textContent = "Please select an answer.";
+        feedback.style.color = "orange";
+        return;
+    }
+    
+    userAnswer = userAnswer.value;
+
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+        feedback.style.color = "green";
+    }
+
+    else {
+        feedback.textContent = "That's incorrect. Try again!";
+        feedback.style.color = "red";
+    }
+
+    
+    
+}
+
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
+
